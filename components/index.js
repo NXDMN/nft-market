35033c5413a33987a72a3a218d4aa779abedf6f1
Norvin <norvintsl@gmail.com>
2022-08-25 20:06:31 +0800

    Initial commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button';
+import DetailsDescription from './DetailsDescription';
+import DetailsBid from './DetailsBid';
+import { SubInfo } from './SubInfo';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDescription,
+  DetailsBid,
+  SubInfo,
+};
